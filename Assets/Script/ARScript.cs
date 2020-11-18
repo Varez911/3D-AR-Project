95be28b50bd51185e5eb8fcab167ca8bf46a5fc5
Varez911 <varez.yeyez@gmail.com>
2020-11-19 02:39:36 +0700

    Added Jean to the other side
    Added Jean model, genshin soundtrack and Idle Mocap Animation to the other side of Id card.

---
+++ Assets/Script/ARScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ARScript : DefaultTrackableEventHandler
+{
+    private TrackableBehaviour trackableImage;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(TrackableBehaviour.Status);
+    }
+
+    public void onTrackableStateChanged(TrackableBehaviour.Status newStatus)
+    {
+        
+    }
+}
---
+++ Assets/Script/Status.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Status : MonoBehaviour
+{
+    public Animator animator;
+    public AudioSource audioOpening;
+
+    public void soundOn()
+    {
+        Debug.Log("Tracked");
+        audioOpening.Play();
+    }
+
+    public void soundOff()
+    {
+        Debug.Log("Lost Tracking");
+        audioOpening.Stop();
+    }
+}
